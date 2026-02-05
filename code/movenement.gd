f25acff24735ec77fdbd4d0fce10d327281d2abc
fabianovich <fabianvanacoley@gmail.com>
2026-02-05 22:03:04 +0100

    multiplayer

--- code/movenement.gd
+++ code/movenement.gd
@@ -4,8 +4,12 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+
 
 func _physics_process(delta: float) -> void:
+	#if !is_multiplayer_authority(): return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
