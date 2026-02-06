18558c1d63cd0fd9e9e8e0cf158aaefa4aaed8a9
fabianovich <fabianvanacoley@gmail.com>
2026-02-06 21:19:12 +0100

    spacing between platforms

--- code/spawnplatforms.gd
+++ code/spawnplatforms.gd
@@ -4,11 +4,24 @@ extends Node
 @onready var y_pos = 700
 @onready var collisionScript = preload("res://code/collisions.gd")
 const y_interval = 150
+var x
+var prevx = 0
 
 
 func _process(_delta):
 	var instance = scene.instantiate()
-	instance.position = Vector2(randf_range(0, 600), y_pos)
+	
+	while true:
+		x = randf_range(0, 600)
+		if abs(x - prevx) < 100:
+			pass
+		else:
+			print (abs(x-prevx))
+			prevx = x
+			
+			break
+		
+	instance.position = Vector2(x, y_pos)
 	instance.set_script(collisionScript)
 	y_pos -= y_interval
 	add_child(instance)
