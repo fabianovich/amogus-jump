a66faab813a53614b889c88fabbcbe3f59d8d4dd
fabianovich <fabianvanacoley@gmail.com>
2026-02-06 21:31:17 +0100

    optimisation >> only spawn platforms when the player height is 800px (for the screen & loading time) below the heighes platform, instead of every 60th of a second

--- code/spawnplatforms.gd
+++ code/spawnplatforms.gd
@@ -1,28 +1,29 @@
 extends Node
 
 @onready var scene = preload("res://scenes/platform.tscn")
-@onready var y_pos = 700
+@onready var character = $"../Character"
+
 @onready var collisionScript = preload("res://code/collisions.gd")
+var y_pos = 700
 const y_interval = 150
 var x
 var prevx = 0
 
 
 func _process(_delta):
-	var instance = scene.instantiate()
-	
-	while true:
-		x = randf_range(0, 600)
-		if abs(x - prevx) < 100:
-			pass
-		else:
-			print (abs(x-prevx))
-			prevx = x
+	if abs(character.position.y - y_pos) < 800:
+		var instance = scene.instantiate()
+		while true:
+			x = randf_range(0, 600)
+			if abs(x - prevx) < 100:
+				pass
+			else:
+				prevx = x
+				break
 			
-			break
-		
-	instance.position = Vector2(x, y_pos)
-	instance.set_script(collisionScript)
-	y_pos -= y_interval
-	add_child(instance)
+		instance.position = Vector2(x, y_pos)
+		instance.set_script(collisionScript)
+		y_pos -= y_interval
+		add_child(instance)
+		print("ligma balls")
 	
