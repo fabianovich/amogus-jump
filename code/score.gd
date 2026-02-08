05569a935c422d84b158c47a23282cb8f7e1e0aa
fabianovich <fabianvanacoley@gmail.com>
2026-02-08 22:50:22 +0100

    spring scene

---
+++ code/score.gd
@@ -0,0 +1,7 @@
+extends Node2D
+@onready var label = $CanvasLayer/Score
+var score = 0
+var y_difference = 0
+func _process(_delta: float) -> void:
+	score += (y_difference)
+	label.text = str(int(score/100))
